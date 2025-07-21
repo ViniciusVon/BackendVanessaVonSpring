3c4c099a57a02e739a8dd2360897e0532f244f43
ViniciusVon <vinnyfut10@gmail.com>
2025-07-20 23:07:02 -0300

    Liberando as origens do cors

--- src/main/java/com/ViniciusVon/backend/controller/ProjetoController.java
+++ src/main/java/com/ViniciusVon/backend/controller/ProjetoController.java
@@ -1,11 +1,13 @@
 package com.ViniciusVon.backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class ProjetoController {
 
